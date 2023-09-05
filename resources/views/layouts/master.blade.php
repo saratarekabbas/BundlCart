9da09ab9d0e7d57ae1104faa5477b5e616d90182
saratarekabbas <52585045+saratarekabbas@users.noreply.github.com>
2023-09-05 23:37:14 +0800

    tailwind css setup

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,14 +1,15 @@
-{{--This file contains the static sections (headers, footers, etc)--}}
-    <!doctype html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
 </head>
 <body>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+
 
 @include('layouts.header')
 
